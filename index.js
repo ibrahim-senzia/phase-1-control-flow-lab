5bcb6cd5b1873db322e8d091ffb9657db870fa0e
ibrahim senzia <ibrahimsenzia@gmail.com>
2024-03-31 10:39:13 +0300

    control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+
+function scuberGreetingForFeet(someValue) {
+  //Write your code here!
+  let result;
+
+  if (someValue <= 400) {
+    result = "This one is on me!";
+  } else if (someValue > 400 && someValue <= 2000) {
+    result = "That will be twenty bucks.";
+  } else if (someValue > 2000 && someValue <= 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else if (someValue > 2500) {
+    result = "No can do.";
+  }
+
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) { 
+// Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+
 }
 
-function switchOnCharmFromTip(){
+
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+    switch (tip) {
+        case "generous":
+            return "Thank you so much.";
+        case "not as generous":
+            return "Thank you.";
+        default:
+            return "Bye.";
+    }
 }
